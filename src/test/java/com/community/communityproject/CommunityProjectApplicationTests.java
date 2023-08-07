d908f49fe963a8b14c6193f3f2a3b56707fbe8e3
apem5186 <apem5186@naver.com>
2023-08-07 20:59:31 +0900

    community.html 페이지네이션 추가

--- src/test/java/com/community/communityproject/CommunityProjectApplicationTests.java
+++ src/test/java/com/community/communityproject/CommunityProjectApplicationTests.java
@@ -1,13 +1,51 @@
 package com.community.communityproject;
 
+import com.community.communityproject.entitiy.board.Board;
+import com.community.communityproject.entitiy.board.Category;
+import com.community.communityproject.entitiy.users.Users;
+import com.community.communityproject.repository.BoardRepository;
+import com.community.communityproject.repository.UserRepository;
+import com.community.communityproject.service.board.BoardService;
+import com.community.communityproject.service.users.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 @SpringBootTest
 class CommunityProjectApplicationTests {
 
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private BoardService boardService;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    @Rollback(value = false)
+    void createBoards() {
+        Users users = userRepository.findByUsername("user01").orElseThrow();
+        for (int i = 1; i <= 150; i++) {
+            String title = String.format("게시글 테스트 제목:[%03d]", i);
+            String content = "게시글 테스트 내용";
+            Board board = Board.builder()
+                    .category(Category.COMMUNITY)
+                    .title(title)
+                    .content(content)
+                    .users(users)
+                    .build();
+            boardRepository.save(board);
+        }
     }
 
 }
